e711da26b00944c1dd23f33d935a2cad0630ba2d
mahadibuet11 <blitheforge@gmail.com>
2024-08-09 04:32:23 +0600

    Updated sql

--- db.sql
+++ db.sql
@@ -1,2 +1,4 @@
-INSERT INTO `paymentmethods` (`methodId`, `methodName`, `methodLogo`, `methodVisibleName`, `methodCallback`, `methodMin`, `methodMax`, `methodFee`, `methodBonusPercentage`, `methodBonusStartAmount`, `methodCurrency`, `methodStatus`, `methodExtras`, `methodPosition`, `methodInstructions`) VALUES
-(71, 'SohojpayBD', 'https://sohojpaybd.com/public/assets/img/logo.gif', 'BD Payment Gateway', 'sohojpaybd', 1, 1000, 0, 0, 0, 'BDT', '1', '{\"api_key\":\"\",\"exchange_rate\":\"110\"}', 1, '');
+INSERT INTO paymentmethods 
+(methodId, methodName, methodLogo, methodVisibleName, methodCallback, methodMin, methodMax, methodFee, methodBonusPercentage, methodBonusStartAmount, methodCurrency, methodStatus, methodExtras, methodPosition, methodInstructions) 
+VALUES 
+(71, 'SohojpayBD', 'https://sohojpaybd.com/public/assets/img/logo.gif', 'BD Payment Gateway', 'sohojpaybd', 1, 1000, 0, 0, 0, 'BDT', '1', '{"api_key":"","exchange_rate":"110"}', 1, '');
